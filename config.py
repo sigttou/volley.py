7d1024d4ad8281a5d063410bfb275e1af11184b4
David Bidner <david.bidner@student.tugraz.at>
2016-11-14 13:17:50 +0100

    ADD: config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Before a match thread change the settings here
+import sys
+
+CONFIG = {
+    'stat_url': "http://volleynet.at/datavolley/2016/women/&D1-" + sys.argv[1] + "_REPORT.htm",
+    'score_url': "http://volleynet.at/datavolley/2016/women/D1-" + sys.argv[1] + "_LIVE.htm",
+    'links': "[volleynet](https://www.volleynet.at)",
+    'stream': "TBA"
+}
